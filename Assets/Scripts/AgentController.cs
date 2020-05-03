55bdfa385d12389373960b0304b9966f5c9b7d04
paszczureKk <dszynszecki@wp.pl>
2020-05-03 16:57:47 +0200

    Advanced world building step 1
    Added dynamics to world building process based on generated data.

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class AgentController : MonoBehaviour
 {
+    private enum Gender
+    {
+        Male,
+        Female,
+        None
+    }
+
     private AgentMovementController amc = null;
     public void Awake()
     {
         amc = this.gameObject.GetComponent<AgentMovementController>();
+    }
+
+    private Gender gender = Gender.None;
+    private bool illness = false;
+
+    public AgentVariables AgentVariables
+    {
+        set
+        {
+            amc.Velocity = new Vector2(value.x, value.y);
+            gender = value.gender == 'M' ? Gender.Male : value.gender == 'F' ? Gender.Female : Gender.None;
+            illness = value.illness;
+        }
     }
 }
